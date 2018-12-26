c5a4cad514a0000a2619aa3538e2f1524960d989
Cipher_Coder@protonmail.com <Cipher_Coder@protonmail.com>
2018-12-25 23:50:19 -0500

    Major Overhaul

--- js/main.js
+++ js/main.js
@@ -7,11 +7,12 @@
 				$('#theMenu').toggleClass('menu-open');
 			});
 
-			document.getElementById("contactMe").addEventListener("click", scrollWin);
-
-			function scrollWin() {
-				window.scrollBy(0, 1200);
-			}
+			var allPanels = $('.section_para').hide();
+    
+  $('.section_header').click(function() {
+      $(this).next().slideToggle();
+      allPanels.not($(this).next()).slideUp();
+  });
 
 
 
