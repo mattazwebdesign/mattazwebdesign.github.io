634d402d14a247830c097061cbe851ed8cbe5f30
Cipher_Coder@protonmail.com <Cipher_Coder@protonmail.com>
2019-02-06 09:57:30 -0500

    cleaned up javascript from menu that no longer worked

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
-;(function(){
+(function() {
+  var allPanels = $(".section_para").hide();
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
-
-			var allPanels = $('.section_para').hide();
-    
-  $('.section_header').click(function() {
-      $(this).next().slideToggle();
-      allPanels.not($(this).next()).slideUp();
+  $(".section_header").click(function() {
+    $(this)
+      .next()
+      .slideToggle();
+    allPanels.not($(this).next()).slideUp();
   });
-
-
-
 })(jQuery);
