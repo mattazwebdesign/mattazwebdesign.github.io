3452eef23e91af4df91ed614ae2683ff4a3bc37f
Cipher_Coder@protonmail.com <Cipher_Coder@protonmail.com>
2019-05-16 19:51:14 -0400

    added chrome extension page for when it is ready

--- js/main.js
+++ js/main.js
@@ -7,4 +7,19 @@
       .slideToggle();
     allPanels.not($(this).next()).slideUp();
   });
+
+  let modal = document.getElementById("myModal");
+  let img = document.getElementById("screenshot");
+  var modalImg = document.getElementById("img01");
+  let caption = document.getElementById("caption");
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    caption.innerHTML = this.alt;
+  };
+
+  let span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
 })(jQuery);
