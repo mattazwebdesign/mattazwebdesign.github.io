230314db2c501dd7df352cb244efa78338ecb8e8
Cipher_Coder@protonmail.com <Cipher_Coder@protonmail.com>
2019-05-21 18:55:26 -0400

    added link to launched firefox extension

--- js/main.js
+++ js/main.js
@@ -22,10 +22,4 @@
   span.onclick = function() {
     modal.style.display = "none";
   };
-
-  document.getElementById("firefox").addEventListener("click", function() {
-    alert(
-      "The Firefox Version is under development right now. The first release will be by the end of May 2019!! Keep checking back or fill out a contact form and I will personally email you when I drop it!"
-    );
-  });
 })(jQuery);
