ce1dbf78436b65000295be105e9c841339819e32
Cipher_Coder@protonmail.com <Cipher_Coder@protonmail.com>
2019-06-05 21:59:00 -0400

    rewrite of blog | made cards and modals instead of collapsable

--- js/main.js
+++ js/main.js
@@ -9,17 +9,22 @@
   });
 
   let modal = document.getElementById("myModal");
-  let img = document.getElementById("screenshot");
-  var modalImg = document.getElementById("img01");
+  let modalImg = document.getElementById("img01");
   let caption = document.getElementById("caption");
-  img.onclick = function() {
+  let img = document.getElementById("screenshot");
+  if (img != null) {
+    img.addEventListener("click", enlarge);
+  }
+  function enlarge() {
     modal.style.display = "block";
     modalImg.src = this.src;
     caption.innerHTML = this.alt;
-  };
+  }
 
   let span = document.getElementsByClassName("close")[0];
-  span.onclick = function() {
-    modal.style.display = "none";
-  };
+  if (span != null) {
+    span.onclick = function() {
+      modal.style.display = "none";
+    };
+  }
 })(jQuery);
